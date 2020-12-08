7625c7bbafff0eec5a838a1e1b3d90d8ef3f5910
Ross <ug.ross@hotmail.com>
2020-12-08 22:09:55 +0200

    Add Queue, Add Queue tests

--- src/data-structures/Queue/Queue.ts
+++ src/data-structures/Queue/Queue.ts
@@ -3,24 +3,30 @@ import { LinkedList } from '../LinkedList/LinkedList';
 export class Queue<T> {
   private storage = new LinkedList<T>();
 
+  /**
+   * Add a new element to the end of the queue.
+   * This element will be processed after all elements that are already in the queue.
+   */
   public enqueue(value: T): void {
-    this.storage.prepend(value);
+    this.storage.append(value);
   }
 
+  /** Remove the element that is at the front of queue. */
   public dequeue(): T | null {
-    return this.storage.deleteTail()?.value || null;
+    return this.storage.deleteHead()?.value || null;
   }
 
+  /** Read the element at the front of the queue without removing it */
   public peek(): T {
     return this.storage.getFirst()?.value || null;
   }
 
-  public size(): number {
-    return this.size();
+  public isEmpty(): boolean {
+    return !this.storage.getFirst();
   }
 
-  public isEmpty(): boolean {
-    return !!this.storage.getFirst();
+  public size(): number {
+    return this.storage.toArray().length;
   }
 
   public toString(callback?: (val: T) => string): string {
--- src/data-structures/Queue/__tests__/Queue.test.ts
+++ src/data-structures/Queue/__tests__/Queue.test.ts
@@ -2,13 +2,111 @@ import { Queue } from '../Queue';
 
 describe('Queue', () => {
   describe('Access', () => {
-    it('should return the latest item in queue', () => {
+    it('should return head of the queue', () => {
       const queue = new Queue<string>();
       queue.enqueue('a');
       queue.enqueue('b');
       queue.enqueue('c');
 
-      expect(queue.peek()).toEqual('c');
+      expect(queue.peek()).toEqual('a');
+    });
+
+    it('should return null if queue is empty', () => {
+      const queue = new Queue<string>();
+
+      expect(queue.peek()).toBeNull();
+    });
+  });
+
+  describe('Insertion', () => {
+    it('should add item to queue', () => {
+      const valueToPush = 1;
+      const queue = new Queue<number>();
+      queue.enqueue(valueToPush);
+
+      expect(queue.peek()).toBe(valueToPush);
+    });
+  });
+
+  describe('Deletion', () => {
+    it('should return null if queue is empty', () => {
+      const queue = new Queue();
+
+      expect(queue.dequeue()).toBeNull();
+    });
+
+    it('should remove item that was added first and return it', () => {
+      const queue = new Queue<string>();
+      const firstAddedItem = 'hello';
+
+      queue.enqueue(firstAddedItem);
+      queue.enqueue('world');
+      queue.enqueue('!');
+
+      expect(queue.dequeue()).toBe(firstAddedItem);
+    });
+  });
+
+  describe('isEmpty()', () => {
+    let queue;
+
+    beforeEach(() => {
+      queue = new Queue<number>();
+    });
+
+    it('should return true if queue is empty', () => {
+      const queue = new Queue();
+
+      expect(queue.isEmpty()).toEqual(true);
+    });
+
+    it('should return false if queue is not empty', () => {
+      queue.enqueue(1);
+
+      expect(queue.isEmpty()).toEqual(false);
+    });
+  });
+
+  describe('size()', () => {
+    let queue;
+
+    beforeEach(() => {
+      queue = new Queue<number>();
+    });
+
+    it('should return 0 if queue is empty', () => {
+      expect(queue.size()).toEqual(0);
+    });
+
+    it('should return correct queue size', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+
+      expect(queue.size()).toEqual(3);
+    });
+  });
+
+  describe('toString()', () => {
+    const data = ['1', '2', '3'];
+    let queue: Queue<string>;
+
+    beforeEach(() => {
+      queue = new Queue<string>();
+      data.forEach((value) => queue.enqueue(value));
+    });
+
+    it('should convert values to string using default method', () => {
+      const expectedResult = data.toString();
+
+      expect(queue.toString()).toEqual(expectedResult);
+    });
+
+    it('should convert values to string using callback', () => {
+      const callback = (val) => val + 'a';
+      const expectedResult = data.map(callback).toString();
+
+      expect(queue.toString(callback)).toEqual(expectedResult);
     });
   });
 });
